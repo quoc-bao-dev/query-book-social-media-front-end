5be3346557db5984855b0fce9d77df4e4461aca6
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2025-01-26 08:27:56 +0700

    fea: user profile page

--- src/app/(main)/(profile)/layout.tsx
+++ src/app/(main)/(profile)/layout.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react';
 
 const Layout = ({ children }: PropsWithChildren) => {
-    return <div>{children}</div>;
+    return <div className="mx-auto w-fit mt-5">{children}</div>;
 };
 
 export default Layout;
--- src/components/Layout/SearchResultRow.tsx
+++ src/components/Layout/SearchResultRow.tsx
@@ -1,32 +1,39 @@
 import { getFirstCharacter } from '@/utils/nameUtilts';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import Link from 'next/link';
 
 type SearchResultRowProps = {
+    id: string;
     avatar?: string;
     name: string;
     title: string;
     onClick?: () => void;
 };
 const SearchResultRow = ({
+    id,
     avatar,
     name,
     title,
     onClick,
 }: SearchResultRowProps) => {
     return (
-        <div
-            className="flex gap-2 p-2 rounded-lg hover:bg-gray-200/60 cursor-pointer"
-            onClick={onClick}
-        >
-            <Avatar className="h-[48px] w-[48px] rounded-lg">
-                <AvatarImage src={avatar} className="object-cover " />
-                <AvatarFallback>{getFirstCharacter(name)}</AvatarFallback>
-            </Avatar>
-            <div className="">
-                <h3 className="font-semibold text-neutral-800/90 ">{name}</h3>
-                <p className="text-sm text-neutral-600/70 ">{title}</p>
+        <Link href={`/${id}`}>
+            <div
+                className="flex gap-2 p-2 rounded-lg hover:bg-gray-200/60 cursor-pointer"
+                onClick={onClick}
+            >
+                <Avatar className="h-[48px] w-[48px] rounded-lg">
+                    <AvatarImage src={avatar} className="object-cover " />
+                    <AvatarFallback>{getFirstCharacter(name)}</AvatarFallback>
+                </Avatar>
+                <div className="">
+                    <h3 className="font-semibold text-neutral-800/90 ">
+                        {name}
+                    </h3>
+                    <p className="text-sm text-neutral-600/70 ">{title}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
