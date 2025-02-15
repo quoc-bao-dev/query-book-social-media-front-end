3189e27eb064616150c718d1b0c0e7f67d03baa5
Young DOB <youngdob.west@gmail.com>
2025-02-15 13:19:18 +0700

    fea : qna update new

--- src/app/(main)/(qna)/myquestion/partials/MainContentMyQuestion.tsx
+++ src/app/(main)/(qna)/myquestion/partials/MainContentMyQuestion.tsx
@@ -1,133 +1,137 @@
 /* eslint-disable @next/next/no-img-element */
 
-'use client';
-import React, { useEffect, useState } from 'react';
+"use client";
 import {
-    ChatBubbleOvalLeftIcon,
-    HeartIcon,
-    ShareIcon,
-    BookmarkIcon,
-    MagnifyingGlassIcon,
-    ArrowDownIcon,
-    EllipsisVerticalIcon,
-    PencilSquareIcon,
-    TrashIcon,
-    FlagIcon,
-} from '@heroicons/react/24/outline';
+  ArrowDownIcon,
+  BookmarkIcon,
+  ChatBubbleOvalLeftIcon,
+  EllipsisVerticalIcon,
+  FlagIcon,
+  HeartIcon,
+  MagnifyingGlassIcon,
+  PencilSquareIcon,
+  ShareIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
 // import AskQuestionModal from "../../partials/AskQuestionModal";
-import SendIcon from '@/components/icons/SendIcon';
-import AskQuestionButton from '../../partials/AskQuestionButton';
-import { useSearchParams } from 'next/navigation';
-import MainContentAskQuestion from '../../ask-question/partials/MainContentAskQuestion';
-import { useGetMyQuestion } from '@/queries/question';
+import SendIcon from "@/components/icons/SendIcon";
+import { useSearchParams } from "next/navigation";
+import MainContentAskQuestion from "../../ask-question/partials/MainContentAskQuestion";
+import AskQuestionButton from "../../partials/AskQuestionButton";
+import { useGetMyQuestion } from "@/queries/question";
+import { useAuth } from "@/store/authSignal";
+import SetCurUserProfileSignal from "@/app/(main)/(profile)/partials/SetCurUserProfileSignal";
 
-const posts = [
-    {
-        author: 'jcole.lamar',
-        timeAgo: '10h ago',
-        title: 'How to learn TypeScript?',
-        content:
-            'I’m a web developer who has been working with JavaScript for quite a while...',
-        imageUrl:
-            'https://www.didongmy.com/vnt_upload/news/05_2024/anh-27-meme-dang-yeu-didongmy.jpg',
-        authorImage:
-            'https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg',
-        comments: 200,
-        likes: 2300,
-    },
-    {
-        author: 'jcole.lamar',
-        timeAgo: '10h ago',
-        title: 'How to use if...else in JavaScript?',
-        content:
-            'I’m currently learning JavaScript, and I’ve been working with conditional statements...',
-        imageUrl:
-            'https://vietnampfa.com/wp-content/uploads/2024/10/meme-meo-cuoi-6r0oJnQ.webp',
-        authorImage:
-            'https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg',
-        comments: 150,
-        likes: 1800,
-    },
-    {
-        author: 'jcole.lamar',
-        timeAgo: '10h ago',
-        title: 'How to learn TypeScript?',
-        content:
-            'Cyber Security is a rapidly growing field, and it can seem daunting for beginners...',
-        imageUrl:
-            'https://gcs.tripi.vn/public-tripi/tripi-feed/img/477714EJG/anh-mo-ta.png',
-        authorImage:
-            'https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg',
-        comments: 300,
-        likes: 2100,
-    },
-    {
-        author: 'jcole.lamar',
-        timeAgo: '10h ago',
-        title: 'How to learn TypeScript?',
-        content:
-            'Cyber Security is a rapidly growing field, and it can seem daunting for beginners...',
-        imageUrl:
-            'https://topdev.vn/blog/wp-content/uploads/2019/06/vs-code-theme-Night-Owl.png',
-        authorImage:
-            'https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg',
-        comments: 300,
-        likes: 2100,
-    },
+// const posts = [
+//   {
+//     author: "jcole.lamar",
+//     timeAgo: "10h ago",
+//     title: "How to learn TypeScript?",
+//     content:
+//       "I’m a web developer who has been working with JavaScript for quite a while...",
+//     imageUrl:
+//       "https://www.didongmy.com/vnt_upload/news/05_2024/anh-27-meme-dang-yeu-didongmy.jpg",
+//     authorImage:
+//       "https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg",
+//     comments: 200,
+//     likes: 2300,
+//   },
+//   {
+//     author: "jcole.lamar",
+//     timeAgo: "10h ago",
+//     title: "How to use if...else in JavaScript?",
+//     content:
+//       "I’m currently learning JavaScript, and I’ve been working with conditional statements...",
+//     imageUrl:
+//       "https://vietnampfa.com/wp-content/uploads/2024/10/meme-meo-cuoi-6r0oJnQ.webp",
+//     authorImage:
+//       "https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg",
+//     comments: 150,
+//     likes: 1800,
+//   },
+//   {
+//     author: "jcole.lamar",
+//     timeAgo: "10h ago",
+//     title: "How to learn TypeScript?",
+//     content:
+//       "Cyber Security is a rapidly growing field, and it can seem daunting for beginners...",
+//     imageUrl:
+//       "https://gcs.tripi.vn/public-tripi/tripi-feed/img/477714EJG/anh-mo-ta.png",
+//     authorImage:
+//       "https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg",
+//     comments: 300,
+//     likes: 2100,
+//   },
+//   {
+//     author: "jcole.lamar",
+//     timeAgo: "10h ago",
+//     title: "How to learn TypeScript?",
+//     content:
+//       "Cyber Security is a rapidly growing field, and it can seem daunting for beginners...",
+//     imageUrl:
+//       "https://topdev.vn/blog/wp-content/uploads/2019/06/vs-code-theme-Night-Owl.png",
+//     authorImage:
+//       "https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg",
+//     comments: 300,
+//     likes: 2100,
+//   },
 
-    {
-        author: 'jcole.lamar',
-        timeAgo: '10h ago',
-        title: 'How to learn TypeScript?',
-        content:
-            'Cyber Security is a rapidly growing field, and it can seem daunting for beginners...',
-        imageUrl:
-            'https://gcs.tripi.vn/public-tripi/tripi-feed/img/477714EJG/anh-mo-ta.png',
-        authorImage:
-            'https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg',
-        comments: 300,
-        likes: 2100,
-    },
-    {
-        author: 'jcole.lamar',
-        timeAgo: '10h ago',
-        title: 'How to learn TypeScript?',
-        content:
-            'Cyber Security is a rapidly growing field, and it can seem daunting for beginners...',
-        imageUrl:
-            'https://carre.edu.vn/wp-content/uploads/2024/12/anh-meme-hai-huoc-che-e1735373827733.webp',
-        authorImage:
-            'https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg',
-        comments: 300,
-        likes: 2100,
-    },
-    {
-        author: 'jcole.lamar',
-        timeAgo: '10h ago',
-        title: 'How to learn TypeScript?',
-        content:
-            'Cyber Security is a rapidly growing field, and it can seem daunting for beginners...',
-        imageUrl:
-            'https://cdn.caohockinhte.edu.vn/wp-content/uploads/2024/08/hinh-tau-hai-bua-1.jpg',
-        authorImage:
-            'https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg',
-        comments: 300,
-        likes: 2100,
-    },
-    // Add more posts as needed...
-];
+//   {
+//     author: "jcole.lamar",
+//     timeAgo: "10h ago",
+//     title: "How to learn TypeScript?",
+//     content:
+//       "Cyber Security is a rapidly growing field, and it can seem daunting for beginners...",
+//     imageUrl:
+//       "https://gcs.tripi.vn/public-tripi/tripi-feed/img/477714EJG/anh-mo-ta.png",
+//     authorImage:
+//       "https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg",
+//     comments: 300,
+//     likes: 2100,
+//   },
+//   {
+//     author: "jcole.lamar",
+//     timeAgo: "10h ago",
+//     title: "How to learn TypeScript?",
+//     content:
+//       "Cyber Security is a rapidly growing field, and it can seem daunting for beginners...",
+//     imageUrl:
+//       "https://carre.edu.vn/wp-content/uploads/2024/12/anh-meme-hai-huoc-che-e1735373827733.webp",
+//     authorImage:
+//       "https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg",
+//     comments: 300,
+//     likes: 2100,
+//   },
+//   {
+//     author: "jcole.lamar",
+//     timeAgo: "10h ago",
+//     title: "How to learn TypeScript?",
+//     content:
+//       "Cyber Security is a rapidly growing field, and it can seem daunting for beginners...",
+//     imageUrl:
+//       "https://cdn.caohockinhte.edu.vn/wp-content/uploads/2024/08/hinh-tau-hai-bua-1.jpg",
+//     authorImage:
+//       "https://hips.hearstapps.com/hmg-prod/images/j_cole_photo_by_isaac_brekken_wireimage_getty_503069628.jpg",
+//     comments: 300,
+//     likes: 2100,
+//   },
+//   // Add more posts as needed...
+// ];
 
 const MainContentMyQuestion = () => {
   const [showMore, setShowMore] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
-
+  const { user } = useAuth();
   const param = useSearchParams();
   const mode = param.get("mode");
   const [searchTerm, setSearchTerm] = useState("");
 
-  const filteredPosts = posts.filter((post) =>
-    post.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const { data } = useGetMyQuestion();
+  const filteredPosts =
+    data?.filter((post) =>
+      post.title.toLowerCase().includes(searchTerm.toLowerCase())
+    ) || [];
 
   const toggleShowMore = () => {
     setShowMore(!showMore);
@@ -144,6 +148,7 @@ const MainContentMyQuestion = () => {
   }
   return (
     <div className="mx-auto p-4 bg-background max-h-full pt-[65px]">
+      <SetCurUserProfileSignal user={user} />
       {/* Thanh tìm kiếm */}
       <div className="mb-6 relative">
         <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
@@ -157,49 +162,32 @@ const MainContentMyQuestion = () => {
           className="w-full pl-10 px-4 py-2 placeholder-neutral-500 placeholder:opacity-70 border border-none rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
         />
       </div>
-      <div className="flex justify-between items-center mb-4">
-        {/* Button Back */}
-        <div></div>
-
-    if (mode === 'ask') {
-        return <MainContentAskQuestion />;
-    }
-    return (
-        <div className="mx-auto p-4 bg-background max-h-full pt-[65px]">
-            {/* Thanh tìm kiếm */}
-            <div className="mb-6 relative">
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <MagnifyingGlassIcon className="h-5 w-5 text-[#00A76F]" />
-                </div>
-                <input
-                    type="text"
-                    placeholder="Search by author or tag..."
-                    value={searchTerm}
-                    className="w-full pl-10 px-4 py-2 placeholder-neutral-500 placeholder:opacity-70 border border-none rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                />
-            </div>
-            <div className="flex justify-between items-center mb-4">
-                {/* Button Back */}
-                <div></div>
+      <div className="flex justify-end items-center mb-4">
+        <AskQuestionButton />
+      </div>
 
       {/* Render bài viết */}
+      {/* {data?.map((iteam) => (
+        <p key={iteam._id}>{iteam.title}</p>
+      ))} */}
       {filteredPosts.length > 0 ? (
         filteredPosts
           .slice(0, showMore ? filteredPosts.length : 4)
-          .map((post, index) => (
+          .map((post) => (
             <div
-              key={index}
+              key={post._id}
               className="rounded-lg shadow-lg p-4 mb-6 border border-border bg-card"
             >
               <div className="flex items-center justify-between mt-3 ">
                 <div className="flex items-center space-x-2">
                   <img
-                    src={post.authorImage}
+                    src={user?.avatarUrl}
                     alt="user"
                     className="w-10 h-10 rounded-full"
                   />
+
                   <h4 className="font-semibold text-neutral-900">
-                    {post.author}
+                    {user?.fullName}
                   </h4>
                   <p className="text-sm text-neutral-500">{post.timeAgo}</p>
                 </div>
@@ -235,7 +223,7 @@ const MainContentMyQuestion = () => {
               </h2>
 
               {/* Content */}
-              <p className="mt-2 text-lg text-neutral-600">{post.content}</p>
+              <p className="mt-2 text-lg text-neutral-600">{post.question}</p>
 
               {/* Image */}
               {post.imageUrl && (
@@ -261,7 +249,7 @@ const MainContentMyQuestion = () => {
               {/* Reply Section */}
               <div className="mt-4 flex items-center gap-3">
                 <img
-                  src={post.authorImage}
+                  src="http://localhost:3008/1739160152820-214983997-ech.jpg"
                   alt="user"
                   className="w-10 h-10 rounded-full"
                 />
