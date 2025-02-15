e9e50b3464bebbedc1e05dfd29dd6b14071b5a61
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2025-02-15 13:23:13 +0700

    fea: add friend with socket

--- src/app/(main)/(profile)/partials/CoverPage.tsx
+++ src/app/(main)/(profile)/partials/CoverPage.tsx
@@ -1,143 +1,159 @@
-"use client";
+'use client';
 
-import Camera from "@/components/icons/Camera";
-import CreditCard from "@/components/icons/CreditCard";
-import DocumentText from "@/components/icons/Document-text";
-import Rss from "@/components/icons/Rss";
-import UserPlus from "@/components/icons/User-plus";
-import { useAuth } from "@/store/authSignal";
-import Link from "next/link";
-import { sCurUserProfileSignal } from "../signal/curUserProfileSignal";
+import Camera from '@/components/icons/Camera';
+import CreditCard from '@/components/icons/CreditCard';
+import DocumentText from '@/components/icons/Document-text';
+import Rss from '@/components/icons/Rss';
+import UserPlus from '@/components/icons/User-plus';
+import { useAuth } from '@/store/authSignal';
+import Link from 'next/link';
+import { sCurUserProfileSignal } from '../signal/curUserProfileSignal';
+import { useSendRequestMutation } from '@/queries/friend';
 
 const CoverPage = () => {
-  const { user: userMe } = useAuth();
-  const { user } = sCurUserProfileSignal.use();
-  const isMe = user?.id === userMe?.id;
-  const targetLink = isMe ? "/me" : `/${user?.id}`;
-  const profileLink = isMe ? "/me/profile" : `/${user?.id}/profile`;
+    const { user: userMe } = useAuth();
+    const { user } = sCurUserProfileSignal.use();
 
-  return (
-    <div className="relative rounded-b-2xl overflow-hidden">
-      <div className="bg-[url('/images/bia2.jpg')] object-cover bg-center h-[250px] relative bg-gray-500">
-        <div className="absolute bottom-0 pl-[calc(16px+180px)] w-[calc(100%-16px+180px)]">
-          <div className="pl-3 py-4">
-            <h1 className="text-4xl font-semibold text-white ">
-              {user?.fullName}
-            </h1>
-            <h3 className="text-gray-500">{user?.jobTitle?.title}</h3>
-            <p className="font-semibold text-base text-neutral-900"></p>
-            <div className="flex gap-1 mt-2 ">
-              <div className="size-8 rounded-full bg-gray-500">
-                <img
-                  src="/images/kaka.png"
-                  alt="Friend 1"
-                  className="w-full h-full rounded-full object-cover"
-                />
-              </div>
-              <div className="size-8 rounded-full bg-gray-500">
-                <img
-                  src="/images/ronaldo.png"
-                  alt="Friend 1"
-                  className="w-full h-full rounded-full object-cover"
-                />
-              </div>
-              <div className="size-8 rounded-full bg-gray-500">
-                <img
-                  src="/images/that.png"
-                  alt="Friend 1"
-                  className="w-full h-full rounded-full object-cover"
-                />
-              </div>
+    const { mutateAsync } = useSendRequestMutation();
+
+    const handleSendRequest = () => {
+        if (!user) return;
+        mutateAsync(user?.id);
+    };
+
+    const isMe = user?.id === userMe?.id;
+    const targetLink = isMe ? '/me' : `/${user?.id}`;
+    const profileLink = isMe ? '/me/profile' : `/${user?.id}/profile`;
+
+    return (
+        <div className="relative rounded-b-2xl overflow-hidden">
+            <div className="bg-[url('/images/bia2.jpg')] object-cover bg-center h-[250px] relative bg-gray-500">
+                <div className="absolute bottom-0 pl-[calc(16px+180px)] w-[calc(100%-16px+180px)]">
+                    <div className="pl-3 py-4">
+                        <h1 className="text-4xl font-semibold text-white ">
+                            {user?.fullName}
+                        </h1>
+                        <h3 className="text-gray-500">
+                            {user?.jobTitle?.title}
+                        </h3>
+                        <p className="font-semibold text-base text-neutral-900"></p>
+                        <div className="flex gap-1 mt-2 ">
+                            <div className="size-8 rounded-full bg-gray-500">
+                                <img
+                                    src="/images/kaka.png"
+                                    alt="Friend 1"
+                                    className="w-full h-full rounded-full object-cover"
+                                />
+                            </div>
+                            <div className="size-8 rounded-full bg-gray-500">
+                                <img
+                                    src="/images/ronaldo.png"
+                                    alt="Friend 1"
+                                    className="w-full h-full rounded-full object-cover"
+                                />
+                            </div>
+                            <div className="size-8 rounded-full bg-gray-500">
+                                <img
+                                    src="/images/that.png"
+                                    alt="Friend 1"
+                                    className="w-full h-full rounded-full object-cover"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div className="px-4">
+                <div className="relative">
+                    {/* Avatar */}
+                    <div className="absolute translate-y-[-80%] size-[170px] rounded-full bg-muted overflow-hidden">
+                        {user?.avatarUrl ? (
+                            <img
+                                src={user.avatarUrl}
+                                alt="Avatar"
+                                className="w-full h-full object-cover rounded-full shadow-lg"
+                            />
+                        ) : (
+                            <div className="w-full h-full flex items-center justify-center rounded-full shadow-lg text-neutral-900 font-bold text-4xl">
+                                {user?.firstName?.charAt(0).toUpperCase()}
+                            </div>
+                        )}
+
+                        {user?.id === userMe?.id ? (
+                            <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center bg-black/40 opacity-0 hover:opacity-100 transition-opacity duration-300 h-1/5 rounded-b-full cursor-pointer">
+                                <Camera className="w-6 h-6 text-white mb-1" />
+                            </div>
+                        ) : (
+                            <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center bg-black/40 opacity-0 hover:opacity-100 transition-opacity duration-300 h-1/5 rounded-b-full cursor-pointer">
+                                <span className="text-white text-xs">
+                                    Xem ảnh
+                                </span>{' '}
+                                {/* Hoặc cái gì đó khác */}
+                            </div>
+                        )}
+                    </div>
+                    {/* Avatar */}
+                </div>
+            </div>
+
+            <div className="bg-card  flex justify-end items-center ">
+                <div className="flex justify-center space-x-2 relative">
+                    <Link href={profileLink} className="block">
+                        <div className="relative flex flex-col items-center py-3 px-3 group cursor-pointer">
+                            <div className="flex items-center space-x-2">
+                                <CreditCard className="fill-primary-500" />
+                                <span className="text-base font-bold text-neutral-800 ">
+                                    Hồ sơ
+                                </span>
+                            </div>
+                            {/* Thêm border-bottom khi hover mà không thay đổi kích thước */}
+                            <div className="absolute bottom-0 left-0 w-full h-0.5 bg-primary-500 opacity-0 group-hover:opacity-100 transition-all transform group-hover:scale-x-100 rounded-md"></div>
+                        </div>
+                    </Link>
+
+                    <Link href={targetLink} className="block">
+                        <div className="relative flex flex-col items-center py-3 px-3 group cursor-pointer">
+                            <div className="flex items-center space-x-2">
+                                <DocumentText className="fill-primary-500" />
+                                <span className="text-base font-bold text-neutral-800">
+                                    Bài viết
+                                </span>
+                            </div>
+                            {/* Thêm border-bottom khi hover mà không thay đổi kích thước */}
+                            <div className="absolute bottom-0 left-0 w-full h-0.5 bg-primary-500 opacity-0 group-hover:opacity-100 transition-all transform group-hover:scale-x-100 rounded-md"></div>
+                        </div>
+                    </Link>
+
+                    {user?.id != userMe?.id && (
+                        <div className="relative flex flex-col items-center cursor-pointer py-3 px-3">
+                            <div
+                                className="flex items-center space-x-2"
+                                onClick={handleSendRequest}
+                            >
+                                <UserPlus className="fill-primary-500" />
+                                <span className="text-base font-bold text-neutral-800">
+                                    Thêm bạn bè
+                                </span>
+                            </div>
+                        </div>
+                    )}
+
+                    {user?.id != userMe?.id && (
+                        <div className="relative flex flex-col items-center py-3 px-3 group cursor-pointer">
+                            <div className="flex items-center space-x-2">
+                                <Rss className="fill-primary-500" />
+                                <span className="text-base font-bold text-neutral-800">
+                                    Theo dõi
+                                </span>
+                            </div>
+                            {/* Thêm border-bottom khi hover mà không thay đổi kích thước */}
+                            <div className="absolute bottom-0 left-0 w-full h-0.5 bg-primary-500 opacity-0 group-hover:opacity-100 transition-all transform group-hover:scale-x-100 rounded-md"></div>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="px-4">
-        <div className="relative">
-          {/* Avatar */}
-          <div className="absolute translate-y-[-80%] size-[170px] rounded-full bg-muted overflow-hidden">
-            {user?.avatarUrl ? (
-              <img
-                src={user.avatarUrl}
-                alt="Avatar"
-                className="w-full h-full object-cover rounded-full shadow-lg"
-              />
-            ) : (
-              <div className="w-full h-full flex items-center justify-center rounded-full shadow-lg text-neutral-900 font-bold text-4xl">
-                {user?.firstName?.charAt(0).toUpperCase()}
-              </div>
-            )}
-
-            {user?.id === userMe?.id ? (
-              <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center bg-black/40 opacity-0 hover:opacity-100 transition-opacity duration-300 h-1/5 rounded-b-full cursor-pointer">
-                <Camera className="w-6 h-6 text-white mb-1" />
-              </div>
-            ) : (
-              <div className="absolute bottom-0 left-0 right-0 flex items-center justify-center bg-black/40 opacity-0 hover:opacity-100 transition-opacity duration-300 h-1/5 rounded-b-full cursor-pointer">
-                <span className="text-white text-xs">Xem ảnh</span>{" "}
-                {/* Hoặc cái gì đó khác */}
-              </div>
-            )}
-          </div>
-          {/* Avatar */}
-        </div>
-      </div>
-
-      <div className="bg-card  flex justify-end items-center ">
-        <div className="flex justify-center space-x-2 relative">
-          <Link href={profileLink} className="block">
-            <div className="relative flex flex-col items-center py-3 px-3 group cursor-pointer">
-              <div className="flex items-center space-x-2">
-                <CreditCard className="fill-primary-500" />
-                <span className="text-base font-bold text-neutral-800 ">
-                  Hồ sơ
-                </span>
-              </div>
-              {/* Thêm border-bottom khi hover mà không thay đổi kích thước */}
-              <div className="absolute bottom-0 left-0 w-full h-0.5 bg-primary-500 opacity-0 group-hover:opacity-100 transition-all transform group-hover:scale-x-100 rounded-md"></div>
-            </div>
-          </Link>
-
-          <Link href={targetLink} className="block">
-            <div className="relative flex flex-col items-center py-3 px-3 group cursor-pointer">
-              <div className="flex items-center space-x-2">
-                <DocumentText className="fill-primary-500" />
-                <span className="text-base font-bold text-neutral-800">
-                  Bài viết
-                </span>
-              </div>
-              {/* Thêm border-bottom khi hover mà không thay đổi kích thước */}
-              <div className="absolute bottom-0 left-0 w-full h-0.5 bg-primary-500 opacity-0 group-hover:opacity-100 transition-all transform group-hover:scale-x-100 rounded-md"></div>
-            </div>
-          </Link>
-
-          {user?.id != userMe?.id && (
-            <div className="relative flex flex-col items-center cursor-pointer py-3 px-3">
-              <div className="flex items-center space-x-2">
-                <UserPlus className="fill-primary-500" />
-                <span className="text-base font-bold text-neutral-800">
-                  Thêm bạn bè
-                </span>
-              </div>
-            </div>
-          )}
-
-          {user?.id != userMe?.id && (
-            <div className="relative flex flex-col items-center py-3 px-3 group cursor-pointer">
-              <div className="flex items-center space-x-2">
-                <Rss className="fill-primary-500" />
-                <span className="text-base font-bold text-neutral-800">
-                  Theo dõi
-                </span>
-              </div>
-              {/* Thêm border-bottom khi hover mà không thay đổi kích thước */}
-              <div className="absolute bottom-0 left-0 w-full h-0.5 bg-primary-500 opacity-0 group-hover:opacity-100 transition-all transform group-hover:scale-x-100 rounded-md"></div>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default CoverPage;
--- src/provider/SocketProvider.tsx
+++ src/provider/SocketProvider.tsx
@@ -3,26 +3,41 @@
 import { config } from '@/config';
 import { useAuth } from '@/store/authSignal';
 import { createContext, useContext, useEffect, useState } from 'react';
+import { signify } from 'react-signify';
 import { io, Socket } from 'socket.io-client';
 
 type SocketContextType = {
     socket: Socket | null;
 };
 
+export const sSocket = signify<SocketContextType>({
+    socket: null,
+});
+
 const massageSocket = createContext<SocketContextType>({ socket: null });
 const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-    const {user} = useAuth()
+    const { user } = useAuth();
     const [socket, setSocket] = useState<Socket | null>(null);
     useEffect(() => {
         if (!user) return;
         const socketInstance = io(config.MESSAGE_SOCKET_URL, {
             query: {
                 userId: user.id,
-            }
+            },
         });
         socketInstance.on('connect', () => {
             setSocket(socketInstance);
-            console.log('connected', socketInstance.id);
+        });
+
+        const socketServer = io(config.SOCKET_URL, {
+            query: {
+                userId: user.id,
+            },
+        });
+
+        socketServer.on('connect', () => {
+            sSocket.set((n) => (n.value.socket = socketServer));
+            console.log('[connected]', socketServer.id);
         });
 
         return () => {
