7850fe3073dc9494de73acda1e2c57c5da0d7f02
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2025-02-17 12:13:06 +0700

    fea: clear post

--- src/queries/post.ts
+++ src/queries/post.ts
@@ -14,7 +14,7 @@ const getPost = ({ pageParam = 1 }: { pageParam: number }) =>
     axiosClient.get<HttpResponse<PostResponse>>('/posts', {
         params: {
             page: pageParam,
-            limit: 10,
+            limit: 5,
         },
     });
 
