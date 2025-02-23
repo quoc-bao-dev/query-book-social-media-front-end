dac688df91180c07824ba18b558f8c2cd1c907a4
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2025-02-23 16:37:47 +0700

    fix sign up

--- src/httpClient/httpClient.ts
+++ src/httpClient/httpClient.ts
@@ -1,85 +1,87 @@
+import { HttpError } from '@/types/common';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface HttpClient {
-    get<T = any>(url: string, headers?: HeadersInit): Promise<T>;
-    post<T = any>(url: string, data?: any, headers?: HeadersInit): Promise<T>;
-    patch<T = any>(url: string, data?: any, headers?: HeadersInit): Promise<T>;
-    delete<T = any>(url: string, headers?: HeadersInit): Promise<T>;
+  get<T = any>(url: string, headers?: HeadersInit): Promise<T>;
+  post<T = any>(url: string, data?: any, headers?: HeadersInit): Promise<T>;
+  patch<T = any>(url: string, data?: any, headers?: HeadersInit): Promise<T>;
+  delete<T = any>(url: string, headers?: HeadersInit): Promise<T>;
 }
 
 const httpClient: HttpClient = {
-    async get<T>(url: string, headers: HeadersInit = {}): Promise<T> {
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                ...headers,
-                'Content-Type': 'application/json',
-            },
-        });
+  async get<T>(url: string, headers: HeadersInit = {}): Promise<T> {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`GET request failed: ${response.statusText}`);
-        }
+    if (!response.ok) {
+      throw new Error(`GET request failed: ${response.statusText}`);
+    }
 
-        return response.json();
-    },
+    return response.json();
+  },
 
-    async post<T>(
-        url: string,
-        data: unknown,
-        headers: HeadersInit = {}
-    ): Promise<T> {
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                ...headers,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
+  async post<T>(
+    url: string,
+    data: unknown,
+    headers: HeadersInit = {},
+  ): Promise<T> {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
-        if (!response.ok) {
-            throw new Error(`POST request failed: ${response.statusText}`);
-        }
+    if (!response.ok) {
+      throw (await response.json()) as HttpError;
+    }
 
-        return response.json();
-    },
+    return response.json();
+  },
 
-    async patch<T>(
-        url: string,
-        data: unknown,
-        headers: HeadersInit = {}
-    ): Promise<T> {
-        const response = await fetch(url, {
-            method: 'PATCH',
-            headers: {
-                ...headers,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
+  async patch<T>(
+    url: string,
+    data: unknown,
+    headers: HeadersInit = {},
+  ): Promise<T> {
+    const response = await fetch(url, {
+      method: 'PATCH',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
 
-        if (!response.ok) {
-            throw new Error(`PATCH request failed: ${response.statusText}`);
-        }
+    if (!response.ok) {
+      throw new Error(`PATCH request failed: ${response.statusText}`);
+    }
 
-        return response.json();
-    },
+    return response.json();
+  },
 
-    async delete<T>(url: string, headers: HeadersInit = {}): Promise<T> {
-        const response = await fetch(url, {
-            method: 'DELETE',
-            headers: {
-                ...headers,
-                'Content-Type': 'application/json',
-            },
-        });
+  async delete<T>(url: string, headers: HeadersInit = {}): Promise<T> {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json',
+      },
+    });
 
-        if (!response.ok) {
-            throw new Error(`DELETE request failed: ${response.statusText}`);
-        }
+    if (!response.ok) {
+      throw new Error(`DELETE request failed: ${response.statusText}`);
+    }
 
-        return response.json();
-    },
+    return response.json();
+  },
 };
 
 export default httpClient;
