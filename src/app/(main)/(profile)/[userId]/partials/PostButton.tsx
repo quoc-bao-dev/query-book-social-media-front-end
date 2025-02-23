7ab23b7efa1774db04f2ecf91959e040fb98e203
TranPhanAnhDung <dungtpa2004@gmail.com>
2025-02-23 18:15:12 +0700

    fea: Update profileprofile

--- src/app/(main)/(profile)/[userId]/partials/PostButton.tsx
+++ src/app/(main)/(profile)/[userId]/partials/PostButton.tsx
@@ -2,9 +2,11 @@
 
 import DocumentText from '@/components/icons/Document-text';
 import { usePathname } from 'next/navigation';
+import { sCurUserProfileSignal } from '../../signal/curUserProfileSignal';
 
 const PostButton = () => {
   const pathname = usePathname(); // Lấy đường dẫn hiện tại
+  const { user } = sCurUserProfileSignal.use();
 
   return (
     <div className='relative flex flex-col items-center py-3 px-3 group cursor-pointer'>
@@ -16,7 +18,12 @@ const PostButton = () => {
       {/* Nếu đường dẫn là /me thì luôn hiển thị border-bottom */}
       <div
         className={`absolute bottom-0 left-0 w-full h-0.5 bg-primary-500 transition-all transform rounded-md 
-          ${pathname === '/me' ? 'opacity-100 scale-x-100' : 'opacity-0'}`}
+            ${
+              pathname === '/me' || pathname === `/${user?.id}`
+                ? 'opacity-100 scale-x-100'
+                : 'opacity-0'
+            }
+          `}
       ></div>
     </div>
   );
