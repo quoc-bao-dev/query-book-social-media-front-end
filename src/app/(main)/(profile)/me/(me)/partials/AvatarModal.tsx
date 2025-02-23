0f9cea65fb1f6b2c4ea381d7de6dc677e95f9cc3
TranPhanAnhDung <dungtpa2004@gmail.com>
2025-02-23 20:54:10 +0700

    fea: update profile close modal uploading image

--- src/app/(main)/(profile)/me/(me)/partials/AvatarModal.tsx
+++ src/app/(main)/(profile)/me/(me)/partials/AvatarModal.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import PlusIcon from "@/components/icons/PlusIcon";
-import { uploadImage } from "@/utils/uploadUtils";
-import { useUpdateUserProfileMutation } from "@/queries/user";
+import { useState } from 'react';
+import PlusIcon from '@/components/icons/PlusIcon';
+import { uploadImage } from '@/utils/uploadUtils';
+import { useUpdateUserProfileMutation } from '@/queries/user';
 // Định nghĩa kiểu cho props
 interface AvatarModalProps {
   isModalOpen: boolean;
@@ -25,71 +25,72 @@ const AvatarModal = ({
     const image = await uploadImage(curImage!);
     const payload = {
       avatar: {
-        type: "image",
-        sourceType: "file",
+        type: 'image',
+        sourceType: 'file',
         fileName: image,
       },
     };
 
     await mutateAsync(payload);
+    setIsModalOpen(false); // Đóng modal sau khi thành công
   };
 
   if (!isModalOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-      <div className="bg-card p-4 rounded-lg w-[500px] h-auto">
+    <div className='fixed inset-0 bg-black/50 flex items-center justify-center z-50'>
+      <div className='bg-card p-4 rounded-lg w-[500px] h-auto'>
         {/* Tiêu đề */}
-        <p className="text-center text-xl pb-4 font-semibold">
+        <p className='text-center text-xl pb-4 font-semibold'>
           Thay ảnh đại diện
         </p>
         {/* Khu vực hiển thị ảnh */}
-        <div className="flex flex-col items-center mb-5">
-          <div className="relative">
+        <div className='flex flex-col items-center mb-5'>
+          <div className='relative'>
             {
-              <div className="w-48 h-48 flex items-center justify-center rounded-full shadow-lg text-neutral-900 font-bold text-4xl">
+              <div className='w-48 h-48 flex items-center justify-center rounded-full shadow-lg text-neutral-900 font-bold text-4xl'>
                 <img
                   src={avtImage}
-                  alt="Avatar"
-                  className="w-48 h-48 object-cover rounded-full"
-                  style={{ imageRendering: "auto" }}
+                  alt='Avatar'
+                  className='w-48 h-48 object-cover rounded-full'
+                  style={{ imageRendering: 'auto' }}
                 />
               </div>
             }
           </div>
 
           {/* Nút tải ảnh lên */}
-          <div className="mt-3 w-auto flex space-x-4">
+          <div className='mt-3 w-auto flex space-x-4'>
             <label
-              htmlFor="avatarUpload"
-              className="flex items-center bottom-0 right-0 bg-blue-500 text-white p-1 px-3 rounded-full shadow cursor-pointer hover:bg-blue-600 transition"
+              htmlFor='avatarUpload'
+              className='flex items-center bottom-0 right-0 bg-blue-500 text-white p-1 px-3 rounded-full shadow cursor-pointer hover:bg-blue-600 transition'
             >
-              <PlusIcon className="w-4 h-4" />
-              <span className="ml-1 text-sm flex">Tải ảnh lên</span>
+              <PlusIcon className='w-4 h-4' />
+              <span className='ml-1 text-sm flex'>Tải ảnh lên</span>
             </label>
           </div>
         </div>
 
         {/* Input chọn file ảnh (ẩn) */}
         <input
-          id="avatarUpload"
-          type="file"
-          accept="image/*"
-          className="hidden"
+          id='avatarUpload'
+          type='file'
+          accept='image/*'
+          className='hidden'
           onChange={(e) => setCurImage(e.target.files![0])}
         />
 
         {/* Nút hành động */}
-        <div className="flex justify-end space-x-3">
+        <div className='flex justify-end space-x-3'>
           <button
             onClick={() => setIsModalOpen(false)}
-            className="h-10 px-5 bg-gray-500 text-white rounded-lg hover:bg-gray-500 transition"
+            className='h-10 px-5 bg-gray-500 text-white rounded-lg hover:bg-gray-500 transition'
           >
             Hủy
           </button>
           <button
             onClick={handleUpload}
-            className="h-10 px-5 bg-primary-500 text-white rounded-lg hover:bg-primary-600 transition"
+            className='h-10 px-5 bg-primary-500 text-white rounded-lg hover:bg-primary-600 transition'
           >
             Lưu
           </button>
