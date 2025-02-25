abf0fa27ba9d41220de7f2a219853d976087c4a2
thatnguyen <thatnguyen8516@gmail.com>
2025-02-25 11:31:00 +0700

    fea: Create Feed

--- src/app/(main)/(home)/(feeds)/partials/ModalCommentDetail.tsx
+++ src/app/(main)/(home)/(feeds)/partials/ModalCommentDetail.tsx
@@ -1,12 +1,11 @@
-"use client";
+'use client';
 
-import Modal from "@/components/common/Modal";
-import { sCommentDetail, useCommentDetail } from "../signal/commentDetail";
-import Post from "./Post";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import PostComment from "./PostComment";
-import { Delete } from "lucide-react";
-import DeleteIcon from "@/components/icons/DeleteIcon";
+import Modal from '@/components/common/Modal';
+import DeleteIcon from '@/components/icons/DeleteIcon';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { sCommentDetail, useCommentDetail } from '../signal/commentDetail';
+import Post from './Post';
+import PostComment from './PostComment';
 
 const ModalCommentDetail = () => {
   const { isOpen, curPost } = sCommentDetail.use();
@@ -15,26 +14,26 @@ const ModalCommentDetail = () => {
 
   return (
     <Modal isOpen={isOpen} onClose={close}>
-      <div className="flex z-20">
-        <div className="w-[700px] rounded-lg bg-gray-200">
-          <div className="relative">
-            <div className="w-full h-12 flex items-center justify-center font-semibold text-xl">
+      <div className='flex z-20'>
+        <div className='w-[700px] rounded-lg bg-gray-200'>
+          <div className='relative'>
+            <div className='w-full h-12 flex items-center justify-center font-semibold text-xl'>
               <p>Bài viết của {curPost?.author.fullName}</p>
             </div>
             <div
               onClick={close}
-              className="bg-neutral-200 flex items-center justify-center absolute top-1 right-1 w-[30px] h-[30px] rounded-full "
+              className='bg-neutral-200 flex items-center justify-center absolute top-1 right-1 w-[30px] h-[30px] rounded-full '
             >
-              <DeleteIcon className="size-8" />
+              <DeleteIcon className='size-8' />
             </div>
           </div>
-          <div className="max-h-[600px] h-[600px]">
-            <ScrollArea className="w-full h-full rounded-xl">
-              <Post post={curPost} mode="onModal" />
-              <ScrollBar orientation="vertical" />
+          <div className='max-h-[600px] h-[600px]'>
+            <ScrollArea className='w-full h-full rounded-xl'>
+              <Post post={curPost} mode='onModal' />
+              <ScrollBar orientation='vertical' />
             </ScrollArea>
           </div>
-          <div className="px-4 py-2">
+          <div className='px-4 py-2'>
             <PostComment postId={curPost.id} />
           </div>
         </div>
