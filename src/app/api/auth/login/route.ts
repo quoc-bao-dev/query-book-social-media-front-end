53f2c4a0976b46ba4bd9a1a2c345096acdd2e788
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2025-02-27 00:43:50 +0700

    fix set cookie

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -18,7 +18,14 @@ export async function POST(request: Request) {
     const response = NextResponse.json({ message: 'Login success' });
 
     // Set cookie
-
+    response.headers.set(
+      'Set-Cookie',
+      `refreshToken=${refreshToken}; HttpOnly; Secure; Max-Age=1296000; Path=/; Domain=${config.API_DOMAIN}; SameSite=None`,
+    );
+    response.headers.set(
+      'Set-Cookie',
+      `accessToken=${accessToken}; HttpOnly; Secure; Max-Age=1296000; Path=/; Domain=${config.API_DOMAIN}; SameSite=None`,
+    );
     setCookies(response).accessToken(accessToken);
     setCookies(response).accessTokenNext(accessToken);
     setCookies(response).refreshToken(refreshToken);
